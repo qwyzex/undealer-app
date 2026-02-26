d59171b047ebb7b40c3c634dddef07d6bf001b9e
qwyzex <qwyzex@gmail.com>
2026-02-26 14:43:31 +0700

    feat: add HomeScreen and update app theme
    Adds a new `HomeScreen` featuring a gradient background, a scrollable list of gradient text, and a custom animated floating bottom navigation bar. Updates `main.dart` to set `HomeScreen` as the initial route and significantly expands the global `ThemeData` with custom styles for input decorations, tab bars, and Cupertino widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'dart:math';
+import 'package:flutter/cupertino.dart';
+import 'package:undealer/screens/home_screen.dart';
+
 import 'widgets/player_tab.dart';
 import 'package:provider/provider.dart';
 import 'package:vector_math/vector_math_64.dart' show Vector3;
@@ -26,13 +29,65 @@ class UndealerApp extends StatelessWidget {
       title: 'Undealer',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.background),
-        appBarTheme: const AppBarThemeData(backgroundColor: AppColors.background),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(backgroundColor: Color(0x00000000)),
         useMaterial3: true,
-        scaffoldBackgroundColor: AppColors.background,
         fontFamily: 'Lexend',
+        scaffoldBackgroundColor: AppColors.background,
+        appBarTheme: const AppBarTheme(backgroundColor: AppColors.background),
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: const TextStyle(color: AppColors.textColorDim),
+          filled: true,
+          fillColor: AppColors.primaryLighter,
+          // Border properties
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(
+              color: Colors.transparent, // Default border color
+              width: 3, // Change thickness here
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(
+              color: AppColors.focusedBorderColor, // Color when focused
+              width: 3, // Change thickness here
+            ),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(
+              color: Colors.red, // Color for error state
+              width: 3, // Change thickness here
+            ),
+          ),
+          contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+        ),
+        bottomAppBarTheme: const BottomAppBarThemeData(color: AppColors.background),
+        textTheme: const TextTheme(bodyMedium: TextStyle(color: AppColors.textColor)),
+        cupertinoOverrideTheme: const NoDefaultCupertinoThemeData(
+          textTheme: CupertinoTextThemeData(
+            textStyle: TextStyle(color: AppColors.textColor, fontFamily: 'Lexend'),
+          ),
+        ),
+        tabBarTheme: const TabBarThemeData(
+          labelColor: AppColors.textColor,
+          unselectedLabelColor: AppColors.textColorDim,
+          indicator: UnderlineTabIndicator(
+            borderSide: BorderSide(color: AppColors.textColor, width: 3.0),
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5)),
+          ),
+        ),
+        primaryColor: AppColors.primary,
       ),
-      home: const TableRoom(title: 'undealer'),
+      // ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: AppColors.background),
+      //   appBarTheme: const AppBarThemeData(backgroundColor: AppColors.background),
+      //   bottomNavigationBarTheme: const BottomNavigationBarThemeData(backgroundColor: Color(0x00000000)),
+      //   useMaterial3: true,
+      //   scaffoldBackgroundColor: AppColors.background,
+      //   fontFamily: 'Lexend',
+      // ),
+      home: const HomeScreen(),
+      // home: const TableRoom(title: 'undealer'),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/cupertino.dart';
+import 'package:scaffold_gradient_background/scaffold_gradient_background.dart';
+import 'package:flutter/material.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+import 'package:undealer/theme/colors.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldGradientBackground(
+      gradient: const LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [Color(0xFFde896b), Color(0xFFF4E9DE)]),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: 30,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) {
+                return Center(
+                  child: GradientText(
+                    "UNDEALER",
+                    colors: [AppColors.background, AppColors.primaryLighter],
+                    style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+                  ),
+                );
+              },
+            ),
+
+            // Floating bottom bar
+            Align(alignment: Alignment.bottomCenter, child: _FloatingBottomBar()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _FloatingBottomBar extends StatefulWidget {
+  @override
+  State<_FloatingBottomBar> createState() => _FloatingBottomBarState();
+}
+
+class _FloatingBottomBarState extends State<_FloatingBottomBar> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      width: 180,
+      margin: EdgeInsets.only(bottom: 15),
+      decoration: BoxDecoration(
+        color: AppColors.background,
+        borderRadius: BorderRadius.circular(40),
+        boxShadow: [BoxShadow(color: Colors.black.withAlpha((0.15 * 255).floor()), blurRadius: 20, offset: const Offset(0, 10))],
+      ),
+      padding: const EdgeInsets.all(8),
+      child: Stack(
+        children: [
+          AnimatedAlign(
+            alignment: selectedIndex == 0 ? Alignment.centerLeft : Alignment.centerRight,
+            duration: Duration(milliseconds: 150),
+            // curve: Curves.elasticOut,
+            curve: Cubic(0.2, 0.9, 0.3, 1.15),
+            child: Container(
+              width: 85,
+              decoration: BoxDecoration(color: AppColors.textColor, borderRadius: BorderRadius.circular(40)),
+            ),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [_buildItem(Icons.home, 0), _buildItem(Icons.settings, 1)]),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildItem(IconData icon, int index) {
+    final isSelected = selectedIndex == index;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      child: Container(
+        // duration: const Duration(milliseconds: 250),
+        // curve: Curves.easeInOut,
+        // decoration: BoxDecoration(color: isSelected ? const Color(0xFF9C5A2E) : Colors.transparent, borderRadius: BorderRadius.circular(30)),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+        child: Icon(icon, color: isSelected ? AppColors.background : AppColors.textColor, size: 25),
+      ),
+    );
+  }
+}
