3e99410b4435573c539f92714615483d1c28342a
qwyzex <qwyzex@gmail.com>
2026-02-27 21:00:18 +0700

    feat: update QuickPlay component UI
    Enhances the `QuickPlay` component by adding a linear gradient background, a "Quick Play!" gradient text title, and decorative `PokerCard` widgets positioned within a `Stack`.

--- lib/components/quick_play.dart
+++ lib/components/quick_play.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+import 'package:undealer/models/suit.dart';
 import 'package:undealer/theme/colors.dart';
+import 'package:undealer/widgets/poker_card.dart';
 
 class QuickPlay extends StatelessWidget {
   @override
@@ -10,9 +13,28 @@ class QuickPlay extends StatelessWidget {
       width: double.infinity,
       height: 200,
       decoration: BoxDecoration(
-        color: AppColors.primaryLighter,
+        gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [AppColors.primaryLighter, Color(0xFFFAFAFA)]),
         borderRadius: BorderRadius.all(Radius.circular(20)),
         boxShadow: [BoxShadow(color: Colors.black26.withAlpha(10), blurRadius: 10, offset: Offset(0, 10))],
+      ),
+      child: Stack(
+        children: [
+          Positioned(left: 20, bottom: -17, child: PokerCard(value: 14, suit: Suit.spades)),
+          Positioned(left: 70, bottom: -12, child: PokerCard(value: 13, suit: Suit.hearts)),
+          Padding(
+            padding: EdgeInsetsGeometry.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GradientText(
+                  "Quick Play!",
+                  colors: [AppColors.textColor, AppColors.textColorDim],
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
