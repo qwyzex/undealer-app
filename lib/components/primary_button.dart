95ae6abe03e1c2d13dbb6c77ba465c02c0bdcd36
qwyzex <qwyzex@gmail.com>
2026-03-01 02:48:17 +0700

    emergency flutter crashes

--- lib/components/primary_button.dart
+++ lib/components/primary_button.dart
@@ -1,35 +1,43 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class PrimaryButton extends StatefulWidget {
+class PrimaryButton extends StatelessWidget {
   final String buttonText;
+  final void Function()? onTap;
 
-  PrimaryButton({required this.buttonText});
-
-  @override
-  _PrimaryButtonState createState() => _PrimaryButtonState(buttonText: buttonText);
-}
-
-class _PrimaryButtonState extends State<PrimaryButton> {
-  final String buttonText;
-
-  _PrimaryButtonState({required this.buttonText});
+  const PrimaryButton({
+    super.key,
+    required this.buttonText,
+    this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // width: double.infinity,
-      width: 160,
-      height: 50,
-      decoration: BoxDecoration(
-        color: Colors.pinkAccent.shade200,
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        boxShadow: [BoxShadow(color: Colors.pinkAccent.shade200.withAlpha(100), blurRadius: 15, offset: Offset(0, 0), spreadRadius: 5)],
-      ),
-      child: Center(
-        child: Text(
-          buttonText,
-          style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 160,
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.pinkAccent.shade200,
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.pinkAccent.shade200.withAlpha(100),
+              blurRadius: 15,
+              offset: const Offset(0, 0),
+              spreadRadius: 5,
+            )
+          ],
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
         ),
       ),
     );
--- lib/components/quick_play.dart
+++ lib/components/quick_play.dart
@@ -3,38 +3,79 @@ import 'package:flutter/material.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
 import 'package:undealer/components/primary_button.dart';
 import 'package:undealer/models/suit.dart';
+import 'package:undealer/screens/table_screen.dart';
 import 'package:undealer/theme/colors.dart';
 import 'package:undealer/widgets/poker_card.dart';
+import 'dart:math' as math;
 
 class QuickPlay extends StatelessWidget {
+  const QuickPlay({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(30),
       width: double.infinity,
-      height: 200,
+      height: 220,
       decoration: BoxDecoration(
         gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [AppColors.primaryLighter, Color(0xFFFAFAFA)]),
         borderRadius: BorderRadius.all(Radius.circular(20)),
-        boxShadow: [BoxShadow(color: Colors.black26.withAlpha(10), blurRadius: 10, offset: Offset(0, 10))],
+        boxShadow: [
+          // BoxShadow(color: Colors.black26, blurRadius: 10, spreadRadius: -20),
+          // BoxShadow(color: Colors.black26.withAlpha(10), blurRadius: 10, offset: Offset(0, 10)),
+        ],
       ),
       child: Stack(
         children: [
-          Positioned(left: 20, bottom: -17, child: PokerCard(value: 14, suit: Suit.spades)),
-          Positioned(left: 70, bottom: -12, child: PokerCard(value: 13, suit: Suit.hearts)),
-          Padding(
-            padding: EdgeInsetsGeometry.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GradientText(
-                  "Quick Play!",
-                  colors: [AppColors.textColor, AppColors.textColorDim],
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
-                ),
-                PrimaryButton(buttonText: "Play"),
-              ],
+          Positioned(
+            left: 20,
+            bottom: -17,
+            child: Transform.rotate(
+              angle: -math.pi / 30,
+              child: PokerCard(value: 14, suit: Suit.spades),
+            ),
+          ),
+          Positioned(
+            left: 70,
+            bottom: -12,
+            child: Transform.rotate(
+              angle: math.pi / 30,
+              child: PokerCard(value: 13, suit: Suit.hearts),
+            ),
+          ),
+          Positioned.fill(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: GradientText(
+                      "Quick Play!",
+                      colors: [AppColors.textColor, AppColors.textColorDim],
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Column(
+                      spacing: 10,
+                      children: [
+                        PrimaryButton(
+                          buttonText: "Play",
+                          onTap: () => {
+                            // route to table screen
+                            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const TableRoom(title: 'undealer'))),
+                          },
+                        ),
+                        PrimaryButton(buttonText: "Settings"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
