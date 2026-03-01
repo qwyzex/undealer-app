8ebfd2c77454fd03ea6b3d7d5d17342ce1662e8e
qwyzex <qwyzex@gmail.com>
2026-03-01 09:20:02 +0700

    feat: add GameOptionsScreen placeholder
    Introduces a new `GameOptionsScreen` stateless widget in `lib/screens/game_options_screen.dart` which currently returns an empty `Container`.

---
+++ lib/screens/game_options_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GameOptionsScreen extends StatelessWidget {
+  const GameOptionsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
